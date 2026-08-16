82981cb05b121a927442f73aeef21c66ec17bf86
vnsunday <truyenspidermanvn@gmail.com>
2026-08-16 20:04:35 +0700

    grid

---
+++ src/applications/electricity/electric.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAXN 10000
+
+void equation() {
+    double azplus[MAXN][2];
+    double azminus[MAXN][2];
+
+    int zone_plus[MAXN][2];
+    int zone_minus[MAXN][2];
+
+    int Pole_Plus;
+    int Pole_Minus;
+
+    // Connected path?
+    int mental_zone[MAXN][2];
+}
+
+void close_circuit() {
+
+}
+
+int main()
+{
+    return 0;
+}
